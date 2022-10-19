16b0e19ff0de41df9ce1437c6b3aea94f53a38b1
JbaptisteLam <jbaptiste.lamouche@gmail.com>
2022-10-19 16:34:46 +0200

    add folder plotcategories and split each type of plot in class

---
+++ vcf2circos/plotcategories/ideogram.py
@@ -0,0 +1,260 @@
+from vcf2circos.plotcategories.plotconfig import Plotconfig
+import inspect
+
+# space, space between ring in option.example.json
+# height hauteur du ring
+# positon position from center
+
+
+list_graph_type = ["majortick", "minortick", "ticklabel"]
+
+
+class Ideogram(Plotconfig):
+    """
+    "scatter":{
+        "pattern":{
+        ...
+    },  "data":{
+        ...
+    }}
+    """
+
+    def __init__(
+        self,
+        filename,
+        options,
+        show,
+        file,
+        radius,
+        sortbycolor,
+        colorcolumn,
+        hovertextformat,
+        trace_car,
+        data,
+        config_ring=None,
+    ):
+        super().__init__(
+            filename,
+            options,
+            show,
+            file,
+            radius,
+            sortbycolor,
+            colorcolumn,
+            hovertextformat,
+            trace_car,
+            data,
+        )
+        self.degreerange = [0, 360]
+        self.showfillcolor = self.cast_bool(True)
+        self.chrannotation = (
+            {
+                "show": "True",
+                "radius": {"R": 1.25},
+                "fonttype": "bold",
+                "textangle": {"angleoffset": 0, "anglelimit": 360},
+                "layout": {
+                    "xref": "x",
+                    "yref": "y",
+                    "showarrow": False,
+                    "font": {"size": 10, "color": "black"},
+                },
+            },
+        )
+        self.customoptions = (
+            {
+                "customlabel": "True",
+                "customspacing": "False",
+                "customcolor": 3,
+            },
+        )
+        self.npoints = (1000,)
+        self.radius = {"R0": 1.0, "R1": 1.1}
+        self.layout = {
+            "type": "path",
+            "opacity": 0.9,
+            "layer": "above",
+            "line": {"color": "gray", "width": 2},
+        }
+        self.majortick = {
+            "show": "True",
+            "spacing": 30000000,
+            "radius": {"R0": 1.1, "R1": 1.125},
+            "layout": {
+                "type": "path",
+                "opacity": 0.9,
+                "layer": "above",
+                "line": {"color": "black", "width": 1},
+            },
+        }
+        self.minortick = {
+            "show": "True",
+            "spacing": 5000000,
+            "radius": {"R0": 1.1, "R1": 1.118},
+            "layout": {
+                "type": "path",
+                "opacity": 0.9,
+                "line": {"color": "black", "width": 0.5},
+            },
+        }
+        self.ticklabel = {
+            "show": "True",
+            "spacing": 30000000,
+            "radius": {"R": 1.16},
+            "textformat": "Mb",
+            "textangle": {"angleoffset": -90, "anglelimit": 360},
+            "layout": {
+                "xref": "x",
+                "yref": "y",
+                "showarrow": False,
+                "font": {
+                    "family": "Times New Roman",
+                    "size": 8,
+                    "color": "black",
+                },
+            },
+        }
+
+        self.ring = config_ring
+        self.variants_position = self.ring["position"]
+        self.variants_ring_space = self.ring["space"]
+        self.variants_ring_height = self.ring["height"]
+
+        self.ringval = [
+                    {
+                        # SNV
+                        "radius": {
+                            "R0": self.variants_position
+                            + (7 * self.variants_ring_space)
+                            + (6 * self.variants_ring_height),
+                            "R1": self.variants_position
+                            + (7 * self.variants_ring_space)
+                            + (7 * self.variants_ring_height),
+                        },
+                        "layout": {
+                            "opacity": 0.1,
+                            "fillcolor": "gray",
+                            "layer": "below",
+                            "line": {"color": "gray", "width": 1},
+                        },
+                    },
+                    {
+                        # level 5
+                        "radius": {
+                            "R0": self.variants_position
+                            + (6 * self.variants_ring_space)
+                            + (5 * self.variants_ring_height),
+                            "R1": self.variants_position
+                            + (6 * self.variants_ring_space)
+                            + (6 * self.variants_ring_height),
+                        },
+                        "layout": {
+                            "opacity": 0.1,
+                            "fillcolor": "lightgrey",
+                            "layer": "below",
+                            "line": {"color": "lightgrey", "width": 1},
+                        },
+                    },
+                    {
+                        # level 4
+                        "radius": {
+                            "R0": self.variants_position
+                            + (5 * self.variants_ring_space)
+                            + (4 * self.variants_ring_height),
+                            "R1": self.variants_position
+                            + (5 * self.variants_ring_space)
+                            + (5 * self.variants_ring_height),
+                        },
+                        "layout": {
+                            "opacity": 0.1,
+                            "fillcolor": "lightgrey",
+                            "layer": "below",
+                            "line": {"color": "lightgrey", "width": 1},
+                        },
+                    },
+                    {
+                        # level 3
+                        "radius": {
+                            "R0": self.variants_position
+                            + (4 * self.variants_ring_space)
+                            + (3 * self.variants_ring_height),
+                            "R1": self.variants_position
+                            + (4 * self.variants_ring_space)
+                            + (4 * self.variants_ring_height),
+                        },
+                        "layout": {
+                            "opacity": 0.1,
+                            "fillcolor": "lightgrey",
+                            "layer": "below",
+                            "line": {"color": "lightgrey", "width": 1},
+                        },
+                    },
+                    {
+                        # level 2
+                        "radius": {
+                            "R0": self.variants_position
+                            + (3 * self.variants_ring_space)
+                            + (2 * self.variants_ring_height),
+                            "R1": self.variants_position
+                            + (3 * self.variants_ring_space)
+                            + (3 * self.variants_ring_height),
+                        },
+                        "layout": {
+                            "opacity": 0.1,
+                            "fillcolor": "white",
+                            "layer": "below",
+                            "line": {"color": "white", "width": 1},
+                        },
+                    },
+                    {
+                        # level 1
+                        "radius": {
+                            "R0": self.variants_position
+                            + (2 * self.variants_ring_space)
+                            + (1 * self.variants_ring_height),
+                            "R1": self.variants_position
+                            + (2 * self.variants_ring_space)
+                            + (2 * self.variants_ring_height),
+                        },
+                        "layout": {
+                            "opacity": 0.1,
+                            "fillcolor": "lightgrey",
+                            "layer": "below",
+                            "line": {"color": "lightgrey", "width": 1},
+                        },
+                    },
+                    {
+                        # level 0
+                        "radius": {
+                            "R0": self.variants_position
+                            + (1 * self.variants_ring_space)
+                            + (0 * self.variants_ring_height),
+                            "R1": self.variants_position
+                            + (1 * self.variants_ring_space)
+                            + (1 * self.variants_ring_height),
+                        },
+                        "layout": {
+                            "opacity": 0.1,
+                            "fillcolor": "lightgrey",
+                            "layer": "below",
+                            "line": {"color": "lightgrey", "width": 1},
+                        },
+                    },
+                ],
+        
+
+    def merge_options(self):
+        dico = {}
+        dico["patch"] = {}
+        # ideo = Ideogram()
+        dico["patch"]["file"] = self.file
+        dico["patch"]["show"] = self.show
+        dico["patch"]["degreerange"] = self.degreerange
+        dico["patch"]["showfillcolor"] = self.showfillcolor
+        dico 
+        #  self.options[attr] = self.attr
+        return dico
+        # Loopable as fuck
+
+
+     
--- vcf2circos/plotcategories/plotconfig.py
+++ vcf2circos/plotcategories/plotconfig.py
@@ -9,12 +9,9 @@ import re
 import subprocess
 import json
 import os
+
 from vcf2circos.vcfreader import VcfReader
 
-print(__file__)
-# sys.path.append(os.path.abspath(os.path.join("../", "demo_data")))
-# sys.path.append(os.path.join(os.path.dirname(__file__), "demo_data"))#
-print(sys.path)
 
 # TODO commons file with utils function maybe one more for globals
 
@@ -26,13 +23,19 @@ class Plotconfig(VcfReader):
     All func based on vcf input
     """
 
-    def __init__(self, filename, options: dict = ...):
-        if os.path.exists(
-            "/maison/lamouche/dev_vcf2circos/demo_data/options.general.json"
-        ):
-            print(True)
-        else:
-            print(False)
+    def __init__(
+        self,
+        filename,
+        options: dict,
+        show: bool,
+        file: dict,
+        radius: dict,
+        sortbycolor: bool,
+        colorcolumn: int,
+        hovertextformat: dict,
+        trace_car: dict,
+        data: list,
+    ):
         super().__init__(filename, options)
         self.default_options = json.load(
             open("/maison/lamouche/dev_vcf2circos/demo_data/options.general.json", "r")
@@ -41,6 +44,24 @@ class Plotconfig(VcfReader):
             self.options["General"]["title"] = os.path.basename(
                 self.get_metadatas().get("filename", "myCircos")
             )
+        self.show = self.cast_bool(show)
+        self.file = file
+        self.radius = radius
+        self.sortbycolor = self.cast_bool(sortbycolor)
+        self.colorcolumn = colorcolumn
+        self.hovertextformat = hovertextformat
+        self.trace_car = trace_car
+        self.data = data
+
+    @staticmethod
+    def cast_bool(value: bool) -> str:
+        if value:
+            return "True"
+        else:
+            return "False"
+
+    def get_file_features():
+        pass
 
     def get_snvindels_overlapping_sv(self):
         pass
@@ -48,10 +69,21 @@ class Plotconfig(VcfReader):
     def vcf_options_default(self):
         pass
 
-    def get_json(self):
+    def get_json(self) -> dict:
         """
         last func to be called, passed to Figure class to generate circos plot (main)
         """
+
+        variants_position = (
+            self.options.get("Variants", {}).get("rings", {}).get("position", 0.5)
+        )
+        variants_ring_height = (
+            self.options.get("Variants", {}).get("rings", {}).get("height", 0.04)
+        )
+        variants_ring_space = (
+            self.options.get("Variants", {}).get("rings", {}).get("space", 0.01)
+        )
+
         pass
 
 
