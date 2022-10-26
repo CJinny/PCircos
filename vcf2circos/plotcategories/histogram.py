e5450dc0496c3367a22a6f395b7665231fb6cc75
jblam <jbaptiste.lamouche@gmail.com>
2022-10-26 18:22:36 +0200

    regarding rangescale definition adapt histogram generation and begin creation of plot for each type of input data

--- vcf2circos/plotcategories/histogram.py
+++ vcf2circos/plotcategories/histogram.py
@@ -6,12 +6,7 @@ import os
 
 class Histogram_(Plotconfig):
     """
-    "scatter":{
-        "pattern":{
-        ...
-    },  "data":{
-        ...
-    }}
+    It need to create one histogram for each SV event FOR EACH SV height (from 0 copy number to 5), which will create the grey band between color dor
     """
 
     def __init__(
@@ -27,6 +22,7 @@ class Histogram_(Plotconfig):
         trace_car,
         data,
         layout,
+        config_ring,
         rangescale,
     ):
         super().__init__(
@@ -42,12 +38,19 @@ class Histogram_(Plotconfig):
             data,
             layout,
         )
+        self.config_ring = config_ring
+        self.variants_position = self.config_ring["position"]
+        self.variants_ring_space = self.config_ring["space"]
+        self.variants_ring_height = self.config_ring["height"]
         self.rangescale = rangescale
+        # corresponding to SNV InDel height 7th ring (after 0 to 5 copy number height)
         self.radius = {
-            "R0": self.options["Variants"]["rings"]["height"] * max(self.rangescale - 1)
-            + 0.02,
-            "R1": self.options["Variants"]["rings"]["height"] * max(self.rangescale)
-            - 0.02,
+            "R0": self.variants_position
+            + (max(self.rangescale) * self.variants_ring_space)
+            + ((max(self.rangescale) + 1) * self.variants_ring_height),
+            "R1": self.variants_position
+            + (max(self.rangescale) * self.variants_ring_space)
+            + ((max(self.rangescale) + 2) * self.variants_ring_height),
         }
         print("#Range", self.rangescale)
         # self.radius = {"R0": 0.90, "R1": 0.92}
@@ -55,13 +58,13 @@ class Histogram_(Plotconfig):
             "path": "",
             "header": "infer",
             "sep": "\t",
-            "dataframe": {"orient": "columns", "data": self.data_histogram_variants()},
+            "dataframe": {"orient": "columns", "data": data},
         }
         self.hovertextformat = " \"<b>{}:{}-{}</b><br>ClinGen informations:<br>{}\".format(a[i,0], a[i,1], a[i,2], a[i,5].replace(';', '<br>').replace('%2C', '<br>   ')) "
         self.trace = {
             "hoverinfo": "text",
             "mode": "markers",
-            "marker": {"size": 5, "symbol": 0, "color": "blue", "opacity": 1},
+            "marker": {"size": 5, "symbol": 0, "color": "gray", "opacity": 1},
         }
         self.layout = {
             "type": "path",
@@ -70,7 +73,9 @@ class Histogram_(Plotconfig):
             "line": {"color": "gray", "width": 5},
         }
 
-    def data_histogram_variants(self):
+    def data_histogram_variants(self) -> dict:
+        # for each sv event regarding copy number
+        file = self.file
         data = {
             "chr_name": [],
             "start": [],
@@ -84,21 +89,38 @@ class Histogram_(Plotconfig):
             data["start"].append(int(val["SV_start"]))
             data["end"].append(int(val["SV_end"]))
             data["val"].append(1)
-            data["color"].append("blue")
+            data["color"].append("grey")
             data["info"].append(
                 ";".join([str(key) + "=" + str(value) for key, value in val.items()])
             )
-        return data
+        file["dataframe"]["data"] = data
+        return file
 
-    def merge_options(self):
-        data = {}
-        data["show"] = self.show
-        data["customfillcolor"] = "False"
-        data["file"] = self.file
-        data["sortbycolor"] = "False"
-        data["colorcolumn"] = 4
-        data["radius"] = self.radius
-        data["hovertextformat"] = self.hovertextformat
-        data["trace"] = self.trace
-        data["layout"] = self.layout
+    def merge_options(self) -> list:
+        for cn in self.rangescale:
+            data = {}
+            data["show"] = self.show
+            data["customfillcolor"] = "False"
+            data["file"] = self.data_histogram_variants()
+            data["sortbycolor"] = "False"
+            data["colorcolumn"] = 4
+            radius = (
+                (
+                    self.variants_position
+                    + (cn * self.variants_ring_space)
+                    + ((cn - 1) * self.variants_ring_height)
+                )
+                + (
+                    self.variants_position
+                    + (cn * self.variants_ring_space)
+                    + (cn * self.variants_ring_height)
+                )
+            ) / 2
+            data["radius"] = {
+                "R0": radius,
+                "R1": radius,
+            }
+            data["hovertextformat"] = self.hovertextformat
+            data["trace"] = self.trace
+            data["layout"] = self.layout
         return data
