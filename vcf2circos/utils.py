0ce04a001dc7ebd408737251b5b7ac57bd321cab
jblam <jbaptiste.lamouche@gmail.com>
2022-11-08 18:15:13 +0100

    update refgene func create now gene transcripts and exons file

--- vcf2circos/utils.py
+++ vcf2circos/utils.py
@@ -72,13 +72,19 @@ def systemcall(command: str) -> list:
             exit()
 
 
-def formatted_refgene(refgene: str, assembly: str, ts: str) -> str:
+def formatted_refgene(refgene: str, assembly: str, ts=None) -> str:
     """
     In case of new version of refgene or new assembly\n
     transcripts: list in string format either 'NM_' or 'NR_ 'or both 'NM_,NR_'
     Took refgene raw file from ucsc curated and create proper exon refgene, WITHOUT UTR(default choice)
+    https://genome.ucsc.edu/cgi-bin/hgTrackUi?g=refSeqComposite&db=hg38
+    ex: python -c 'from foo import hello; print hello()'
+     zcat transcripts.hg38.txt.gz.tmp | grep chr_name > transcripts.hg38.sorted.txt && zcat transcripts.hg38.txt.gz.tmp | grep -v "chr_name" | sort -k1,1V -k2,2n >> transcripts.hg38.sorted.txt
     """
-    transcripts = ts.split(",")
+    if ts is not None:
+        transcripts = ts.split(",")
+    else:
+        transcripts = "NM_"
     df = pd.read_csv(refgene, sep="\t", header=None, compression="infer")
     df.columns = [
         "bin",
@@ -99,86 +105,102 @@ def formatted_refgene(refgene: str, assembly: str, ts: str) -> str:
         "exonFrames",
     ]
     output_genes = os.path.dirname(refgene) + "/genes." + assembly + ".txt.gz.tmp"
+    output_transcripts = (
+        os.path.dirname(refgene) + "/transcripts." + assembly + ".txt.gz.tmp"
+    )
     output_exons = os.path.dirname(refgene) + "/exons." + assembly + ".txt.gz.tmp"
 
     og = os.path.dirname(refgene) + "/genes." + assembly + ".txt.gz"
+    ot = os.path.dirname(refgene) + "/transcripts." + assembly + ".txt.gz"
     oe = os.path.dirname(refgene) + "/exons." + assembly + ".txt.gz"
 
     with gzip.open(output_genes, "wb+") as out_g:
         with gzip.open(output_exons, "wb+") as out_e:
-            out_g.write(
-                bytes(
-                    "\t".join(
-                        [
-                            "chr_name",
-                            "start",
-                            "end",
-                            "val",
-                            "color",
-                            "gene",
-                            "transcript",
-                        ]
+            with gzip.open(output_transcripts, "wb+") as out_t:
+                out_g.write(
+                    bytes(
+                        "\t".join(
+                            [
+                                "chr_name",
+                                "start",
+                                "end",
+                                "val",
+                                "color",
+                                "gene",
+                            ]
+                        )
+                        + "\n",
+                        "UTF-8",
                     )
-                    + "\n",
-                    "UTF-8",
                 )
-            )
-            out_e.write(
-                bytes(
-                    "\t".join(
-                        [
-                            "chr_name",
-                            "start",
-                            "end",
-                            "val",
-                            "color",
-                            "gene",
-                            "exons",
-                            "transcript",
-                        ]
+                out_t.write(
+                    bytes(
+                        "\t".join(
+                            [
+                                "chr_name",
+                                "start",
+                                "end",
+                                "val",
+                                "color",
+                                "gene",
+                                "transcript",
+                            ]
+                        )
+                        + "\n",
+                        "UTF-8",
                     )
-                    + "\n",
-                    "UTF-8",
                 )
-            )
-            for i, row in tqdm(
-                df.iterrows(),
-                total=len(df.index),
-                desc="Formatting refgene file UCSC",
-                leave=False,
-            ):
-                if [row["name"].startswith(ts) for ts in transcripts]:
+                out_e.write(
+                    bytes(
+                        "\t".join(
+                            [
+                                "chr_name",
+                                "start",
+                                "end",
+                                "val",
+                                "color",
+                                "gene",
+                                "exons",
+                                "transcript",
+                            ]
+                        )
+                        + "\n",
+                        "UTF-8",
+                    )
+                )
+                for i, row in tqdm(
+                    df.iterrows(),
+                    total=len(df.index),
+                    desc="Formatting " + os.path.basename(refgene) + " file UCSC",
+                    leave=False,
+                ):
                     out_g.write(
                         bytes(
                             "\t".join(
                                 [
                                     row["chrom"],
-                                    str(row["txStart"]),
-                                    str(row["txEnd"]),
+                                    str(row["cdsStart"]),
+                                    str(row["cdsEnd"]),
                                     "1",
-                                    "lightgray",
-                                    row["name2"],
-                                    row["name"],
+                                    omim_morbid(row["name2"]),
+                                    str(row["name2"]),
                                 ]
                             )
                             + "\n",
                             "UTF-8",
                         )
                     )
-                    for i in range(len(row["exonStarts"].split(",")[:-1])):
-                        exons_start = row["exonStarts"].split(",")
-                        exons_end = row["exonEnds"].split(",")
-                        out_e.write(
+                    if [row["name"].startswith(ts) for ts in transcripts]:
+                        out_t.write(
                             bytes(
                                 "\t".join(
                                     [
                                         row["chrom"],
-                                        str(exons_start[i]),
-                                        str(exons_end[i]),
+                                        str(row["txStart"]),
+                                        str(row["txEnd"]),
                                         "1",
                                         "lightgray",
                                         row["name2"],
-                                        "exon" + str(i + 1),
                                         row["name"],
                                     ]
                                 )
@@ -186,6 +208,27 @@ def formatted_refgene(refgene: str, assembly: str, ts: str) -> str:
                                 "UTF-8",
                             )
                         )
+                        for i in range(len(row["exonStarts"].split(",")[:-1])):
+                            exons_start = row["exonStarts"].split(",")
+                            exons_end = row["exonEnds"].split(",")
+                            out_e.write(
+                                bytes(
+                                    "\t".join(
+                                        [
+                                            row["chrom"],
+                                            str(exons_start[i]),
+                                            str(exons_end[i]),
+                                            "1",
+                                            "lightgray",
+                                            row["name2"],
+                                            "exon" + str(i + 1),
+                                            row["name"],
+                                        ]
+                                    )
+                                    + "\n",
+                                    "UTF-8",
+                                )
+                            )
     # systemcall(
     #    "zcat "
     #    + output_genes
