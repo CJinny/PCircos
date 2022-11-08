8808c8761d871fc5c129b1d724a4738afb4fb728
jblam <jbaptiste.lamouche@gmail.com>
2022-11-08 14:20:47 +0100

    scatter class done working

--- vcf2circos/plotcategories/scatter.py
+++ vcf2circos/plotcategories/scatter.py
@@ -1,21 +1,12 @@
 from vcf2circos.plotcategories.plotconfig import Plotconfig
+from vcf2circos.utils import variants_color, check_data_plot
+from collections import OrderedDict
 from os.path import join as osj
-import pandas as pd
-import os
 import numpy as np
 from pprint import pprint
 
 
 class Scatter_(Plotconfig):
-    """
-    "scatter":{
-        "pattern":{
-        ...
-    },  "data":{
-        ...
-    }}
-    """
-
     def __init__(
         self,
         filename,
@@ -45,17 +36,7 @@ class Scatter_(Plotconfig):
             data,
             layout,
         )
-        # self.df_data = pd.DataFrame.from_dict(self.data).astype(
-        #    {
-        #        "Chromosomes": str,
-        #        "Genes": str,
-        #        "Exons": str,
-        #        "Variants": object,
-        #        "Variants_type": str,
-        #        "CopyNumber": int,
-        #        "Color": str,
-        #    }
-        # )
+        self.hovertextformat = ' "<b>{}:{}</b> | {} > {}<br>{}<br><br>{}".format(a[i,0], a[i,1], a[i,3], a[i,4], a[i,5], a[i,7])'
         self.rangescale = rangescale
         self.config_ring = config_ring
         self.variants_position = self.config_ring["position"]
@@ -71,51 +52,18 @@ class Scatter_(Plotconfig):
             + ((max(self.rangescale) + 2) * self.variants_ring_height),
         }
 
-    def scatter_variants(self):
-        return self.adapt_data()
-
-    def adapt_data(self):
-        # no need end list compare to histogram cuz only dot
-        # data = {
-        #    "chr_name": [],
-        #    "start": [],,
-        #    "val": [],
-        #    "ref": [],
-        #    "alt": [],
-        #    "type": [],
-        #    "color": [],
-        #    "hovertext": [],
-        #    "symbol": [],
-        #    "genes": [],
-        #    "exons": [],
-        # }
-        #            "Chromosomes": [],
-        #    "Genes": [],
-        #    "Exons": [],
-        #    "Record": [],
-        #    "Variants": [],
-        #    "Variants_type": [],
-        #    "CopyNumber": [],
-        #    "Color": []
-        #
-        # keys = ["Chromosome", "Genes", ]
-        # data_f = {x:self.data[x] for x in keys}
-        pass
-
-    def merge_options(self, histo_data):
+    def adapt_data(self, histo_data: list) -> list:
         final = []
         # list of dico from histo class
         for dico in histo_data:
             # key val in each dico
             if dico["trace"]["uid"].startswith("cnv_"):
                 tmp = {}
-                tmpe_se = []
                 for key, val in dico["file"]["dataframe"]["data"].items():
-                    if key not in ["start", "end"]:
+                    if key not in ["start", "end", "color", "genes", "exons"]:
                         tmp[key] = list(np.repeat(val, 2))
-                    elif key == "color":
-                        #utils color match en fonciton du type
-                    #    tmp_se.append(val)
+                        # utils color match en fonciton du type
+                        #    tmp_se.append(val)
                 tmp["start"] = []
                 for s, e in zip(
                     dico["file"]["dataframe"]["data"]["start"],
@@ -123,17 +71,44 @@ class Scatter_(Plotconfig):
                 ):
                     tmp["start"].append(s)
                     tmp["start"].append(e)
-                final.append(tmp)
+                tmp["color"] = [variants_color[var] for var in tmp["type"]]
+                od = OrderedDict()
+                od["chr_name"] = tmp["chr_name"]
+                od["start"] = tmp["start"]
+                od["val"] = tmp["val"]
+                od["ref"] = tmp["ref"]
+                od["alt"] = tmp["alt"]
+                od["type"] = tmp["type"]
+                od["color"] = tmp["color"]
+                for key, val in tmp.items():
+                    if key not in od.keys():
+                        od[key] = val
+                final.append(
+                    [
+                        od,
+                        dico["radius"],
+                        dico["trace"]["uid"],
+                    ]
+                )
+        return final
 
-        pprint(final)
-        print(len(final[0]["chr_name"]))
-        pass
+    def merge_options(self, histo_data):
+        final = []
+        data_list_list = self.adapt_data(histo_data)
 
-    def scatter_cnv_level(self, cn):
+        # return self.scatter_cnv_level(
+        #    data_list_list[0], data_list_list[1], data_list_list[2]
+        # )
+        for data, radius, level in data_list_list:
+            final.append(self.scatter_cnv_level(data, radius, level))
+        # CHECK
+        # for dico_data in final:
+        #    check_data_plot(dico_data)
+        return final
+
+    def scatter_cnv_level(self, data, radius, level):
         d = {}
-
         d["show"] = "True"
-        d["customfillcolor"] = "False"
         d["file"] = {
             "path": "",
             "header": "infer",
@@ -142,33 +117,19 @@ class Scatter_(Plotconfig):
         }
 
         d["sortbycolor"] = "False"
-        d["colorcolumn"] = 7
-        radius = (
-            self.rangescale[cn]
-            + self.rangescale[cn]
-            + self.options["Variants"]["rings"]["height"]
-        ) / 2
-        d["radius"] = {
-            "R0": radius,
-            "R1": radius,
-        }
+        d["colorcolumn"] = 6
+        d["radius"] = radius
         d["hovertextformat"] = self.hovertextformat
         d["trace"] = {
             "hoverinfo": "text",
             "mode": "markers",
             "marker": {
                 "size": 5,
-                "symbol": d_file["dataframe"]["data"]["symbol"],
-                "color": "gray",
-                "opacity": 0.1,
+                "symbol": data["symbol"],
+                "color": data["color"],
+                "opacity": 1,
             },
-            "uid": "cnv_level_" + str(cn),
+            "uid": level,
         }
-        d["layout"] = {
-            "type": "path",
-            "layer": "above",
-            "opacity": 0.1,
-            "fillcolor": "red",
-            "line": {"color": "lightgray", "width": 5},
-        }
+        check_data_plot(data)
         return d
