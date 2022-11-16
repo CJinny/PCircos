70c2cf845c7df2c75d2139a0e86d9f392b80be87
jblam <jbaptiste.lamouche@gmail.com>
2022-11-16 15:13:36 +0100

    fix ring option in config.json, could change number of ring and all size, create upper ring for gene and exons

--- vcf2circos/plotcategories/ring.py
+++ vcf2circos/plotcategories/ring.py
@@ -6,47 +6,49 @@ import numpy as np
 # positon position from center
 
 
-list_graph_type = ["majortick", "minortick", "ticklabel"]
+# list_graph_type = ["majortick", "minortick", "ticklabel"]
 
 
 class Ring(Plotconfig):
-    """
-    "scatter":{
-        "pattern":{
-        ...
-    },  "data":{
-        ...
-    }}
-    """
-
-    def __init__(self, plotconfig):
+    def __init__(self, plotconfig, ring_upper_var=None):
         self.plotconfig = plotconfig
         self.variants_position = self.config_ring["position"]
         self.variants_ring_space = self.config_ring["space"]
         self.variants_ring_height = self.config_ring["height"]
+        self.ring_upper_var = ring_upper_var
 
     def __getattr__(self, item):
         if hasattr(self.plotconfig, item):
             return getattr(self.plotconfig, item)
 
+    # ideogram cyto internal at 1.0
+    def ring(self, R0, R1):
+        return {
+            "radius": {"R0": R0, "R1": R1},
+            "layout": {
+                "opacity": 0.1,
+                "fillcolor": "lightgray",
+                "layer": "below",
+                "line": {"color": "lightgray", "width": 1},
+            },
+        }
+
     def create_ring(self):
         rings_list = []
         # Copy number level
         for i, coeff in enumerate(self.rangescale):
-            rings_list.append(
-                {
-                    "radius": {"R0": coeff, "R1": coeff + self.config_ring["height"],},
-                    "layout": {
-                        "opacity": 0.1,
-                        "fillcolor": "lightgray",
-                        "layer": "below",
-                        "line": {"color": "lightgray", "width": 1},
-                    },
-                },
-            )
+            rings_list.append(self.ring(coeff, coeff + self.variants_ring_height))
+
         rings_list[2]["layout"]["fillcolor"] = "white"
         rings_list[2]["layout"]["line"]["color"] = "white"
 
         rings_list[6]["layout"]["fillcolor"] = "grey"
         rings_list[6]["layout"]["line"]["color"] = "grey"
+        if self.ring_upper_var is not None:
+            R1 = 1.0 - self.variants_ring_space
+            R0 = R1 - self.variants_ring_height
+            for field in self.ring_upper_var:
+                R1 -= self.variants_ring_space - self.variants_ring_height
+                R0 -= self.variants_ring_space - self.variants_ring_height
+                rings_list.append(self.ring(R0, R1))
         return rings_list
