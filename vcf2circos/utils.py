9935a8cbd2f0fdd7f9bc94ce9a6cb09392445d53
Jean-Baptiste Lamouche <jean-baptiste.lamouche@chru-strasbourg.fr>
2023-03-20 16:37:45 +0100

    refseq formatting update, gc creation file

--- vcf2circos/utils.py
+++ vcf2circos/utils.py
@@ -157,9 +157,7 @@ def map_annotations(field_annot):
         return "."
 
 
-def generate_hovertext_var(
-    variants_list, full_annot=None, true_annot=None
-) -> Generator:
+def generate_hovertext_var(variants_list, full_annot=None, true_annot=None) -> Generator:
     # print(self.data["Variants"])
     # print(len(self.data["Variants"]))
     # print(len(self.data["Chromosomes"]))
@@ -198,15 +196,9 @@ def generate_hovertext_var(
                 if i == 15:
                     break
             if not isinstance(pairs[1], list):
-                tmp.append(
-                    ":".join([pairs[0], list(map(map_annotations, [pairs[1]]))[0]])
-                )
+                tmp.append(":".join([pairs[0], list(map(map_annotations, [pairs[1]]))[0]]))
             else:
-                tmp.append(
-                    ": ".join(
-                        [pairs[0], ",".join(list(map(map_annotations, pairs[1])))]
-                    )
-                )
+                tmp.append(": ".join([pairs[0], ",".join(list(map(map_annotations, pairs[1])))]))
         to_add = []
         for items in tmp:
             if len(items) > 40:
@@ -276,9 +268,7 @@ def systemcall(command: str) -> list:
     In case of crash exit code 1 stop script
     """
     print("#[SYS] " + command)
-    p = subprocess.Popen(
-        [command], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
-    )
+    p = subprocess.Popen([command], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     out, err = p.communicate()
     if not err:
         return out.decode("utf8").strip().split("\n")
@@ -308,8 +298,7 @@ def formatted_refgene(refgene: str, assembly: str, ts=None) -> str:
         transcripts = ts.split(",")
     else:
         transcripts = "NM_"
-    df = pd.read_csv(refgene, sep="\t", header=None, compression="infer")
-    df.columns = [
+    cols = [
         "bin",
         "name",
         "chrom",
@@ -327,103 +316,94 @@ def formatted_refgene(refgene: str, assembly: str, ts=None) -> str:
         "cdsEndStat",
         "exonFrames",
     ]
-    output_genes = os.path.dirname(refgene) + "/genes." + assembly + ".txt.gz.tmp"
-    output_transcripts = (
-        os.path.dirname(refgene) + "/transcripts." + assembly + ".txt.gz.tmp"
-    )
-    output_exons = os.path.dirname(refgene) + "/exons." + assembly + ".txt.gz.tmp"
+    df = pd.read_csv(refgene, sep="\t", header=0, compression="infer")
+    assert (
+        len(df.columns) == 16
+    ), "Error in columns format more or less than 16 columns, expected\n\tcols: " + ",".join(cols)
+    df.columns = cols
+    output_genes = os.path.dirname(refgene) + "/genes." + assembly + ".txt"
+    output_transcripts = os.path.dirname(refgene) + "/transcripts." + assembly + ".txt.gz"
+    output_exons = os.path.dirname(refgene) + "/exons." + assembly + ".txt.gz"
 
-    og = os.path.dirname(refgene) + "/genes." + assembly + ".txt.gz"
-    ot = os.path.dirname(refgene) + "/transcripts." + assembly + ".txt.gz"
-    oe = os.path.dirname(refgene) + "/exons." + assembly + ".txt.gz"
+    # og = os.path.dirname(refgene) + "/genes." + assembly + ".txt.gz"
+    # ot = os.path.dirname(refgene) + "/transcripts." + assembly + ".txt.gz"
+    # oe = os.path.dirname(refgene) + "/exons." + assembly + ".txt.gz"
 
-    with gzip.open(output_genes, "wb+") as out_g:
-        with gzip.open(output_exons, "wb+") as out_e:
-            with gzip.open(output_transcripts, "wb+") as out_t:
-                out_g.write(
-                    bytes(
-                        "\t".join(
-                            [
-                                "chr_name",
-                                "start",
-                                "end",
-                                "val",
-                                "color",
-                                "gene",
-                            ]
-                        )
-                        + "\n",
-                        "UTF-8",
+    with gzip.open(output_exons, "wb+") as out_e:
+        with gzip.open(output_transcripts, "wb+") as out_t:
+            out_t.write(
+                bytes(
+                    "\t".join(
+                        [
+                            "chr_name",
+                            "start",
+                            "end",
+                            "val",
+                            "color",
+                            "gene",
+                            "transcript",
+                        ]
                     )
+                    + "\n",
+                    "UTF-8",
                 )
-                out_t.write(
-                    bytes(
-                        "\t".join(
-                            [
-                                "chr_name",
-                                "start",
-                                "end",
-                                "val",
-                                "color",
-                                "gene",
-                                "transcript",
-                            ]
-                        )
-                        + "\n",
-                        "UTF-8",
+            )
+            out_e.write(
+                bytes(
+                    "\t".join(
+                        [
+                            "chr_name",
+                            "start",
+                            "end",
+                            "val",
+                            "color",
+                            "gene",
+                            "exons",
+                            "transcript",
+                        ]
                     )
+                    + "\n",
+                    "UTF-8",
                 )
-                out_e.write(
-                    bytes(
-                        "\t".join(
-                            [
-                                "chr_name",
-                                "start",
-                                "end",
-                                "val",
-                                "color",
-                                "gene",
-                                "exons",
-                                "transcript",
-                            ]
-                        )
-                        + "\n",
-                        "UTF-8",
-                    )
-                )
-                for i, row in tqdm(
-                    df.iterrows(),
-                    total=len(df.index),
-                    desc="Formatting " + os.path.basename(refgene) + " file UCSC",
-                    leave=False,
-                ):
-                    out_g.write(
+            )
+            for i, row in tqdm(
+                df.iterrows(),
+                total=len(df.index),
+                desc="Formatting " + os.path.basename(refgene) + " file UCSC",
+                leave=False,
+            ):
+                if [row["name"].startswith(ts) for ts in transcripts]:
+                    out_t.write(
                         bytes(
                             "\t".join(
                                 [
                                     row["chrom"],
-                                    str(row["cdsStart"]),
-                                    str(row["cdsEnd"]),
+                                    str(row["txStart"]),
+                                    str(row["txEnd"]),
                                     "1",
-                                    str(row["name2"]),
-                                    str(row["name2"]),
+                                    "lightgray",
+                                    row["name2"],
+                                    row["name"],
                                 ]
                             )
                             + "\n",
                             "UTF-8",
                         )
                     )
-                    if [row["name"].startswith(ts) for ts in transcripts]:
-                        out_t.write(
+                    for i in range(len(row["exonStarts"].split(",")[:-1])):
+                        exons_start = row["exonStarts"].split(",")
+                        exons_end = row["exonEnds"].split(",")
+                        out_e.write(
                             bytes(
                                 "\t".join(
                                     [
                                         row["chrom"],
-                                        str(row["txStart"]),
-                                        str(row["txEnd"]),
+                                        str(exons_start[i]),
+                                        str(exons_end[i]),
                                         "1",
                                         "lightgray",
                                         row["name2"],
+                                        "exon" + str(i + 1),
                                         row["name"],
                                     ]
                                 )
@@ -431,60 +411,59 @@ def formatted_refgene(refgene: str, assembly: str, ts=None) -> str:
                                 "UTF-8",
                             )
                         )
-                        for i in range(len(row["exonStarts"].split(",")[:-1])):
-                            exons_start = row["exonStarts"].split(",")
-                            exons_end = row["exonEnds"].split(",")
-                            out_e.write(
-                                bytes(
-                                    "\t".join(
-                                        [
-                                            row["chrom"],
-                                            str(exons_start[i]),
-                                            str(exons_end[i]),
-                                            "1",
-                                            "lightgray",
-                                            row["name2"],
-                                            "exon" + str(i + 1),
-                                            row["name"],
-                                        ]
-                                    )
-                                    + "\n",
-                                    "UTF-8",
-                                )
-                            )
-    # systemcall(
-    #    "zcat "
-    #    + output_genes
-    #    + " | grep 'chr_name' > "
-    #    + og
-    #    + " && zcat "
-    #    + output_genes
-    #    + " | grep -v 'chr_name' "
-    #    + output_genes
-    #    + " | sort -k1,1V -k2,2n > "
-    #    + og
-    # )
-    # systemcall(
-    #    "grep chr_name "
-    #    + output_genes
-    #    + " > "
-    #    + og
-    #    + " && grep -v chr_name "
-    #    + output_genes
-    #    + " | sort -k1,1V -k2,2n > "
-    #    + og
-    # )
-    #
-    # systemcall(
-    #    "grep chr_name "
-    #    + output_exons
-    #    + " > "
-    #    + oe
-    #    + " && grep -v chr_name "
-    #    + output_exons
-    #    + " | sort -k1,1V -k2,2n > "
-    #    + oe
-    # )
+
+    # genes, keep row with the most exon counts
+    with gzip.open(output_genes + ".tmp", "wb+") as out_g:
+        out_g.write(
+            bytes(
+                "\t".join(
+                    [
+                        "chr_name",
+                        "start",
+                        "end",
+                        "val",
+                        "color",
+                        "gene",
+                    ]
+                )
+                + "\n",
+                "UTF-8",
+            )
+        )
+        for gene, df_ in tqdm(
+            df.groupby("name2"), total=len(df.index), desc="Formatting genes file UCSC", leave=False
+        ):
+            df_rows = df_.loc[df_["exonCount"] == df_["exonCount"].max()].loc[
+                :, ["chrom", "cdsStart", "cdsEnd"]
+            ]
+            rows = df_rows.iloc[0, :]
+            out_g.write(
+                bytes(
+                    "\t".join(
+                        [
+                            rows["chrom"],
+                            str(rows["cdsStart"]),
+                            str(rows["cdsEnd"]),
+                            "1",
+                            "lightgray",
+                            gene,
+                        ]
+                    )
+                    + "\n",
+                    "UTF-8",
+                )
+            )
+    systemcall(
+        "zcat "
+        + output_genes
+        + ".tmp | grep 'chr_name' > "
+        + output_genes
+        + " && zcat "+output_genes+".tmp | grep -v 'chr_name' | sort -k1,1V -k2,2n >> "
+        + output_genes+" && /home1/TOOLS/tools/bgzip/current/bin/bgzip "+output_genes
+    )
+    if os.path.exists(output_genes+".tmp"):
+        os.remove(output_genes+".tmp")
+    print("#[INFO] Generated " + ",".join([output_genes, output_exons, output_transcripts]))
     return df
 
 
@@ -503,3 +482,71 @@ def timeit(func):
         return result
 
     return timeit_wrapper
+
+
+def _gc(filedata, span, length, out):
+    """
+    need check in good chroms lists
+    """
+    dico = {}
+    tmp = []
+    val = []
+    chr_valid = []
+    lim = span
+    threshold = span
+    with open(out, "w+") as o:
+        o.write("\t".join(["chr_name", "start", "end", "val", "color"]) + "\n")
+        for i, lines in tqdm(
+            enumerate(filedata),
+            total=int(length[0]),
+            desc="INFO GC percent, bins length: " + str(span) + "",
+        ):
+            if not isinstance(lines, str):
+                lines = lines.decode("utf-8").strip()
+            else:
+                lines = lines.strip()
+            if lines.startswith("variableStep"):
+                chr = lines.split()[1].split("=")[1]
+                continue
+            if chr not in chr_valid:
+            # 500 000
+            start = int(lines.split()[0])
+            stop = int(lines.split()[0]) + 5
+            tmp.append(start)
+            val.append(float(lines.split()[1]) / 20)
+            if lim < start:
+                # first time only
+                lim = start + lim
+            if start >= lim:
+                dico[chr + ":" + str(tmp[0]) + "-" + str(tmp[-1])] = round(
+                    (sum(val) / ((stop - tmp[0]))), 2
+                )
+                o.write(
+                    "\t".join(
+                        [
+                            str(chr),
+                            str(tmp[0]),
+                            str(tmp[-1]),
+                            str(round((sum(val) / ((stop - tmp[0]))), 2)),
+                            "pink",
+                        ]
+                    )
+                    + "\n"
+                )
+                tmp.clear()
+                val.clear()
+                lim += threshold
+
+
+def process_gc_percent(filename: str, out: str) -> str:
+    """ """
+    print(filename)
+    print(out)
+    if filename.endswith(".gz"):
+        with gzip.open(filename, "rb") as bf:
+            length = systemcall("zcat " + filename + " | wc -l")
+            _gc(bf, 200000, length, out)
+    else:
+        f = open(filename, "r")
+        length = systemcall("wc -l " + filename)
+        _gc(f, 200000, length, out)
